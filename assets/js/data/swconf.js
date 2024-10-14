caafb703ead50ae1d7ef148ffb6a47b07ff951ac
Crypto Bambino <live@cryptobambino.com>
2024-10-14 17:37:01 +0100

    add first educational post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1728917450',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1728923778',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/about/', '/educational/', '/categories/', '/tags/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
