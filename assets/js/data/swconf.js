b4607212cab06f0136776a7550a6643423c27696
Crypto Bambino <live@cryptobambino.com>
2026-02-01 21:03:10 +0000

    update UX

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1731749821',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/about/', '/educational/', '/categories/', '/tags/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1769979712',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/reports/', '/categories/', '/about/', '/tags/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
